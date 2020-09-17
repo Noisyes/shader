c241831d0339bb6e1743ea07201d2dce66e6eeec
noisyes <kyf19970408@163.com>
2020-09-17 11:34:12 +0800

    add motionblur

---
+++ UnityShader/Assets/Scripts/MotionBlurWithDepthTexture.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionBlurWithDepthTexture : PostEffectBase
+{
+    public Shader MotionBlurWithDepthShader;
+    private Material MotionBlurWithDepthMaterial;
+    public Material material
+    {
+        get
+        {
+            MotionBlurWithDepthMaterial = CheckShaderAndCreateMaterial(MotionBlurWithDepthShader, MotionBlurWithDepthMaterial);
+            return MotionBlurWithDepthMaterial;
+        }
+    }
+    [Range(0.0f, 1.0f)]
+    public float blurSize = 0.5f;
+    private Camera myCamera;
+    public Camera camera
+    {
+        get
+        {
+            if(myCamera == null)
+            {
+                myCamera = GetComponent<Camera>();
+
+            }
+            return myCamera;
+        }
+    }
+    private Matrix4x4 previousViewProjectionMatrix;
+    private void OnEnable()
+    {
+        camera.depthTextureMode |= DepthTextureMode.Depth;
+    }
+}
